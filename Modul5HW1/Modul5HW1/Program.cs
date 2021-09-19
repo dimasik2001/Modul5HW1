d2faa8db38b41210ed9d1f8e6f45ad0259fa8682
Dimasik2001 <dimonkalibaba@gmail.com>
2021-09-19 13:40:05 +0300

    Empty Project

---
+++ Modul5HW1/Modul5HW1/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Modul5HW1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
